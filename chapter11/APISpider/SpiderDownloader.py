f4d04b8deb4b6e37f76649f5f1e5787cceedcff5
NetworkRanger <17346503142@163.com>
2019-01-28 20:30:21 +0800

    ADD: 完成至第11章

---
+++ chapter11/APISpider/SpiderDownloader.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+# -*- coding:utf-8 -*-
+
+# Author: NetworkRanger
+# Date: 2019/1/28 8:26 PM
+
+import requests
+
+
+class SpiderDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            return None
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'
+        headers = {'User-Agent': user_agent}
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            r.encoding = 'utf-8'
+            return r.text
+        return None
